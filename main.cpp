d982af7c1cab9d6ea3ac63e91362bd14befbb284
jeanhua <2207739460@qq.com>
2024-03-14 17:37:52 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main()
 	game.initWindow(1000, 800);
 	game.setFrame(165);
 	game.addObject("dola", "./Doraemon.png", jhVector2(0, 300), 1, 1,100, 100);
-	game.addObject("xiaoxin", "./xiaoxin.png", jhVector2(700, 300), 1, 1, 100, 100);
-	game.addObject("cat", "./cat.png", jhVector2(800, 245), 1, 1, 100, 100);
-	game.addObject("pkq", "./pkq.png", jhVector2(800, 355), 1, 1, 100, 100);
-	game.getObject("dola")->setSpeed(jhVector2(200, 0));
+	game.addObject("xiaoxin", "./xiaoxin.png", jhVector2(650, 300), 1, 1, 100, 100);
+	game.addObject("cat", "./cat.png", jhVector2(800, 200), 1, 1, 100, 100);
+	game.addObject("pkq", "./pkq.png", jhVector2(800, 400), 1, 1, 100, 100);
+	game.getObject("dola")->setSpeed(jhVector2(200, 10));
 	//¿ªÊ¼ÓÎÏ·
 	game.gameStart();
 }
@@ -20,7 +20,7 @@ int main()
 void Game::update()
 {
 	char dtt[30];
-	sprintf(dtt, "fps:%ld", deltaTime);
+	sprintf(dtt, "deltatime:%ld",deltaTime);
 	cleardevice();
 	outtextxy(0, 0,dtt);
 }
